506583f73c4d762fcd1b521bc2971e8d393e42a9
Aashay Chahande <aashaychahande1717@gmail.com>
2025-10-18 13:02:51 +0530

    Add DataFetcher class with price and returns methods

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,1 +1,17 @@
+class DataFetcher:
+    @staticmethod
+    def get_current_price(ticker):
+        return 100.0
+    @staticmethod
+    def validate_ticker(ticker):
+        return bool(ticker)
+    @staticmethod
+    def get_daily_returns(ticker):
+        import pandas as pd
+        import numpy as np
+        dates = pd.date_range(end=pd.Timestamp.today(), periods=30)
+        returns = np.random.normal(0.001, 0.01, size=len(dates))
+        df = pd.DataFrame({'date': dates, 'returns': returns})
+        df.set_index('date', inplace=True)
+        return df
 
