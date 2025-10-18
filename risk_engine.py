f3e442e0a7d7b3486fce0df094ff672a43a6dfbb
Aashay Chahande <aashaychahande1717@gmail.com>
2025-10-18 12:58:39 +0530

    Implement RiskEngine class with portfolio methods

--- risk_engine.py
+++ risk_engine.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import numpy as np
 
+class RiskEngine:
+    def __init__(self, portfolio):
+        self.portfolio = portfolio
+
+    def get_portfolio_returns(self):
+        dates = pd.date_range(end=pd.Timestamp.today(), periods=30)
+        returns = np.random.normal(0.001, 0.01, size=len(dates))
+        df = pd.DataFrame({'date': dates, 'returns': returns})
+        df.set_index('date', inplace=True)
+        return df
+
+    def get_comprehensive_risk_metrics(self):
+        return {
+            'volatility_annual': 0.15,
+            'sharpe_ratio': 1.2,
+            'beta': 1.0,
+            'max_drawdown': 0.10,
+            'var_95': 0.05,
+            'correlation': 0.7
+        }
